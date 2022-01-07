77a9a013c0e1bd33850b6b493ee20cafeb1924db
Nandini Yarra <yarranandhini1416@gmail.com>
2022-01-07 18:15:57 +0530

    ADDED:analyse and mood response with test case

---
+++ src/test/java/com/bridgelabz/moodanalyser/MoodAnalyserTest.java
@@ -0,0 +1,23 @@
+package test.java.com.bridgelabz.moodanalyser;
+
+iimport org.junit.Test;
+
+import junit.framework.Assert;
+
+public class MoodAnalyserTest {
+	@Test
+	public void givenMood_IsHappy_ShouldReturnSad() {
+		MoodAnalyser analyser = new MoodAnalyser("I am in Sad mood");
+		Assert.assertEquals("SAD",analyser.analyseMood());
+	}
+	@Test
+	public void givenMood_IsAny_ShouldReturnHappy() {
+		MoodAnalyser analyser = new MoodAnalyser("I am in Happy mood");
+		Assert.assertEquals("HAPPY",analyser.analyseMood());
+	}
+	@Test
+	public void givenMood_IsHappy_ShouldReturnException() {
+		MoodAnalyser analyser = new MoodAnalyser();
+		Assert.assertEquals("HAPPY",analyser.analyseMood());
+	}
+}
